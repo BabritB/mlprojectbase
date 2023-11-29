f43dc1bf64d775ab560f96c0a80a19625a405469
BabritB <mail4babrit.com>
2023-11-29 14:01:23 +0530

    Preprocessor picker file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
-import sys
+from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 
 
 if __name__=="__main__":
@@ -11,8 +11,11 @@ if __name__=="__main__":
     try:
         #data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path= data_ingestion.initiate_data_ingestion()
         
+       # data_tranformation_config=DataIngestionConfig()
+        data_tranformation=DataTransformation()
+        data_tranformation.initiate_data_transormation(train_data_path,test_data_path)
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
