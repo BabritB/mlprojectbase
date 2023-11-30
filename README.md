105e180f0d3f97d0039f97904498d0f98fe6cc0e
BabritB <mail4babrit.com>
2023-11-30 12:59:04 +0530

    Train and track with mlflow and dagshub

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## End to End ML Project
+
+## Export via bash for mlflow & dagshub
+MLFLOW_TRACKING_URI=https://dagshub.com/iambabrit/mlprojectbase.mlflow \
+MLFLOW_TRACKING_USERNAME=iambabrit \
+MLFLOW_TRACKING_PASSWORD=3caa38a9f33f65348d7a0081173c1358c1ad5985 \
+python script.py
--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.mlproject.exception import CustomException
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
+from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
 
 if __name__=="__main__":
@@ -13,9 +14,16 @@ if __name__=="__main__":
         data_ingestion=DataIngestion()
         train_data_path,test_data_path= data_ingestion.initiate_data_ingestion()
         
-       # data_tranformation_config=DataIngestionConfig()
+        #data_tranformation_config=DataTransformationConfig()
         data_tranformation=DataTransformation()
-        data_tranformation.initiate_data_transormation(train_data_path,test_data_path)
+        #data_tranformation.initiate_data_transormation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_tranformation.initiate_data_transormation(train_data_path,test_data_path)
+
+         ## Model Training
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
